ae2710c5806cdab38f79d6e677e3ba6e2a7f9b5e
Vlad Gobr <vladgobr@yahoo.com>
2021-04-28 23:40:42 +0300

    add file

---
+++ Script_DZ.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd Skp
+mkdir Script_1 Script_2 Script_3
+cd Script_3
+touch One.txt Two.txt Three.txt First.json Second.json
+mkdir 1 2 3
+ls -l
+mv First.json Three.txt ~/Desktop/Skp/Script_2
